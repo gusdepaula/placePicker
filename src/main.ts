0a74dddd890adb6a24a2674a429d50709be8fbcb
Gustavo de Paula <gdepaula@gmail.com>
2025-03-26 15:20:08 -0300

    Add logging interceptor to HTTP client for incoming responses

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(
   request: HttpRequest<unknown>,
@@ -15,8 +17,19 @@ function loggingInterceptor(
   //   const req = request.clone({
   //     headers: request.headers.set('X-Debug-Header', 'Testing'),
   //   });
+  console.log('[Outgoing Request]');
   console.log(request);
-  return next(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, {
